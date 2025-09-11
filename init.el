a7726a70d2cd697b459b5db2303b30236a4e7a82
Marcus <marcus@r38.se>
2025-09-11 23:22:16 +0200

    edit some text

--- init.el
+++ init.el
@@ -146,7 +146,7 @@
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
-       ;;hy                ; readability of scheme w/ speed of python
+       ;; hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        (java +lsp)       ; the poster child for carpal tunnel syndrome
